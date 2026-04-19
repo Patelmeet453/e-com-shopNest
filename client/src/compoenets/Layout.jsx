0776058f72768fad32db1e9515c206f244ec081a
Meet-patel <pmeet1963@gmail.com>
2026-04-19 13:12:58 +0530

    Add Tippy tooltip to VoiceNavButton

--- client/src/compoenets/Layout.jsx
+++ client/src/compoenets/Layout.jsx
@@ -3,6 +3,8 @@ import { Outlet } from "react-router-dom";
 import VoiceNavButton from "./VoiceNavButton";
 import ChatBot from "./Chatbot";
 import Footer from "./Footer";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
 
 const Layout = () => {
   return (
@@ -15,9 +17,19 @@ const Layout = () => {
       </main>
 
       {/* VOICE NAV FLOATING BUTTON */}
-      <div className="fixed bottom-24 right-6 z-50">
-        <VoiceNavButton />
-      </div>
+    <div className="fixed bottom-24 right-6 z-50">
+  <Tippy
+    content="🎤  Use voice to navigate pages or find products by name or category"
+      placement="auto"   // ✅ auto adjust
+  trigger="mouseenter focus click"
+    animation="shift-away"
+    delay={[200, 100]}
+  >
+    <div>
+      <VoiceNavButton />
+    </div>
+  </Tippy>
+</div>
 
       <div className="fixed bottom-6 right-12 z-50">
          {/* <ChatBot /> */}
