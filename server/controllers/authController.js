1a95d18f7b175e2d138abfafd093d2953c68467c
Meet-patel <pmeet1963@gmail.com>
2026-04-19 12:54:22 +0530

    update authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -10,7 +10,7 @@ const setTokenCookie = (res, userId) => {
   res.cookie("token", token, {
     httpOnly: true, // ❗ cannot be accessed by JS
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
